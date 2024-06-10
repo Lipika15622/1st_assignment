f5cea2e712be26b4f23c82151ded94c5ecdcbb25
Lipika15622 <lipika.cse15@outlook.com>
2024-06-10 16:00:54 +0800

    Update problem3(string_reversed_without_wordOrder).php

--- problem3(string_reversed_without_wordOrder).php
+++ problem3(string_reversed_without_wordOrder).php
@@ -2,25 +2,20 @@
 
 function reversed_Sentence($sentence) {
     $spilt_word = explode(' ', $sentence); 
-  //  $reversed_word = array();
-        
-    foreach ( $spilt_word as $word) {
+  foreach ( $spilt_word as $word) {
         $reversed_word = '';
         for ($i = strlen($word) - 1; $i >= 0; $i--) {
             $reversed_word .= $word[$i];
         }
         $reversed_words[] = $reversed_word;
     }
-
-    $reversed_Sentence = implode(' ', $reversed_words); 
+   $reversed_Sentence = implode(' ', $reversed_words); 
     return $reversed_Sentence;
 }
 
-
-
-$original_Sentence = readline("Enter the string: ");
-$reversed_Sentence = reversed_Sentence($original_Sentence);
-echo "Original Sentence: " . $original_Sentence . "\n";
+$Given_Sentence = readline("Enter the string: ");
+$reversed_Sentence = reversed_Sentence($Given_Sentence);
+echo "Original Sentence: " . $Given_Sentence . "\n";
 echo "Reversed Sentence: " . $reversed_Sentence;
 
 ?>
