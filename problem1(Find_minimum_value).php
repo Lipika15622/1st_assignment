9f3b548abe1fcfae1178e98b5c76687c1997dae4
Lipika15622 <lipika.cse15@outlook.com>
2024-06-10 15:56:10 +0800

    Update problem1(Find_minimum_value).php

--- problem1(Find_minimum_value).php
+++ problem1(Find_minimum_value).php
@@ -5,23 +5,16 @@
  
 function minimum_value($arr)
 {
-	
-	
-		for($j=1; $j<count($arr); $j++)
+	for($j=1; $j<count($arr); $j++)
 		{
             if($arr[$j]<0)
             $arr[$j] = -1 * $arr[$j];
 		}
-        
-    
-
-	$minimum_value = $arr[0];
+        $minimum_value = $arr[0];
 	  
 	  	for($i=1; $i<count($arr); $i++)
 	{
-
-	
-		if(	$minimum_value > $arr[$i])
+		if($minimum_value > $arr[$i])
 		
 		 { 
 		 	$minimum_value = $arr[$i];
@@ -29,17 +22,15 @@ function minimum_value($arr)
 		 }
 	}
 	if($minimum_value<0)
-     {
+        {
 		return -1 * $minimum_value;
 	}
 	else
 	{
-		
 		return  $minimum_value;
 
 	}
 
-	
 }
 echo "Minimum value is : " .minimum_value($arr = explode(' ', readline()));
 ?>
