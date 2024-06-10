a9a127a5a3865452903b97f5b6bc2c63b4396f91
Lipika15622 <lipika.cse15@outlook.com>
2024-06-10 15:58:18 +0800

    Update problem2(file).php

--- problem2(file).php
+++ problem2(file).php
@@ -4,22 +4,15 @@
 
  $filePath = 'file.txt';
  
- 
  $fileContents = file_get_contents($filePath);
  
- 
- //$words = preg_split('/\s+/', $fileContents, -1, PREG_SPLIT_NO_EMPTY);
  $words = explode(' ',$fileContents );
  
- 
  $wordCount = 0;
- 
  
  for ($i = 0; $i < count($words); $i++) {
      $wordCount++;
  }
- 
- 
  echo "The file contains " . $wordCount . " words.";
 
 ?>
